9fbdc6eb2036c1a54e2af8b815349e255e99b11c
高志鑫 <gaozhixin@meituan.com>
2019-09-29 15:44:14 +0800

    implement a homemade filter

--- haskell/strain/src/Strain.hs
+++ haskell/strain/src/Strain.hs
@@ -1,7 +1,12 @@
 module Strain (keep, discard) where
 
 discard :: (a -> Bool) -> [a] -> [a]
-discard p xs = filter (not . p) xs
+discard p xs = myFilter (not . p) xs
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep p xs = filter p xs
+keep p xs = myFilter p xs
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter p xs = case xs of
+    x:tail -> if p x then x:(myFilter p tail) else myFilter p tail
+    _ -> []
