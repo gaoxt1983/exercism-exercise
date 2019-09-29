eef38438eb9cd9dfe09ce384c6c071ea3340fdf7
高志鑫 <gaozhixin@meituan.com>
2019-09-29 15:47:41 +0800

    remove duplication

--- haskell/strain/src/Strain.hs
+++ haskell/strain/src/Strain.hs
@@ -1,12 +1,9 @@
 module Strain (keep, discard) where
 
 discard :: (a -> Bool) -> [a] -> [a]
-discard p xs = myFilter (not . p) xs
+discard p xs = keep (not . p) xs
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep p xs = myFilter p xs
-
-myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p xs = case xs of
-    x:tail -> if p x then x:(myFilter p tail) else myFilter p tail
+keep p xs = case xs of
+    x:tail -> if p x then x:(keep p tail) else keep p tail
     _ -> []
