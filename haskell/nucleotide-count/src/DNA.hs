d3368fca2898ecb718c58e1329b9190d53b97b5e
高志鑫 <gaozhixin@meituan.com>
2019-09-30 17:22:50 +0800

    slightly remove fmap number

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -6,7 +6,7 @@ import Text.Read
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Enum, Read)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = fmap (unionsWith (+)) $ fmap (\x -> init : x) $ fmap (\x -> Prelude.map (\y -> fromList [(y, 1)]) x) $ traverse translate xs
+nucleotideCounts xs = fmap ((unionsWith (+)) . (\x -> init : x) . (\x -> Prelude.map (\y -> fromList [(y, 1)]) x)) $ traverse translate xs
     where
         translate c = readEither [c] :: (Either String Nucleotide)
         init = fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
