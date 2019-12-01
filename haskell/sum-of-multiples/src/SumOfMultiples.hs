1f885641ffba185e9ce60183c7a99e1468ef86b8
高志鑫 <gaoxt1983@163.com>
2019-12-01 19:07:34 +0800

    improved

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,8 +1,9 @@
 module SumOfMultiples (sumOfMultiples) where
 
 import Data.List
+import qualified Data.Set as Set
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum . nub . concat $ map (\x -> takeWhile (< limit) [x * y | y <- [1..]]) effectiveFactors
+sumOfMultiples factors limit = sum . Set.unions $ map (\x -> Set.fromAscList $ takeWhile (< limit) [x * y | y <- [1..]]) effectiveFactors
     where
         effectiveFactors = filter (/= 0) factors
